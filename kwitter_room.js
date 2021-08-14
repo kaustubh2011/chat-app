888e47ce8d099f5738ec805cd642b52685f34ceb
kaustubh2011 <81859002+kaustubh2011@users.noreply.github.com>
2021-08-14 16:25:51 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -27,9 +27,20 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
        Room_names = childKey;
       //Start code
 
-      //End code
+      console.log("roomname- ",+Room_names);
+row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >"+ Room_names +"</div><hr>";
+     document.getElementById("output").innerHTML+=row;
+//End code
+
       });});}
 getData();
+
+function redirectToRoomName(name){
+      console.log(name);
+      localStorage.setItem("roomname",name);
+      window.location="kwitter_page.html";
+}
+
 
 function logout()
 {
